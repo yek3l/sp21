80b6edf27a16c6d66f9f485d289934ee84ac08cf
maxsonyang <maxsonyang@berkeley.edu>
2020-12-14 12:57:36 -0800

    rolling back to bootstrap 4, fixing styling on quicklinks

--- src/components/schedule-components/calendar.jsx
+++ src/components/schedule-components/calendar.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import '@fortawesome/fontawesome-free/css/all.css';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import "../../styles/sassets/schedule.scss";
 
 
@@ -13,9 +16,10 @@ export default class Calendar extends React.Component {
                 <FullCalendar
                     plugins={[ 
                       timeGridPlugin,
-                      googleCalendarPlugin
+                      googleCalendarPlugin,
+                      bootstrapPlugin
                     ]}
-
+                    themeSystem="bootstrap"
                     initialView="timeGridWeek"
                 />
               </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,9 +19,13 @@ class Home extends React.Component {
       <div>
         <Helmet>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossorigin="anonymous"
+          />
         </Helmet>
         <div>
             <NavigationBar />
