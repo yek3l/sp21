98e51dc76a6f317dfdaeede3c3185e4621cb6ded
maxsonyang <maxsonyang@berkeley.edu>
2020-12-14 13:01:32 -0800

    reverting dashboard back to fluid container

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,7 +29,7 @@ class Home extends React.Component {
         </Helmet>
         <div>
             <NavigationBar />
-            <div className="container content-body">
+            <div className="container-fluid content-body">
               <h1>
                 Dashboard
               </h1>
