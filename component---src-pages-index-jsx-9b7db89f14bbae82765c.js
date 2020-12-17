9b75d429f4bec331bd96864e493d2cbd449a1f0a
maxsonyang <maxsonyang@berkeley.edu>
2020-12-17 00:48:38 -0800

    Updates

--- component---src-pages-index-jsx-9b7db89f14bbae82765c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{Dtc0:function(e,t,n){"use strict";n.r(t);var a=n("dI71"),r=n("q1tI"),o=n.n(r),c=n("Wbzz"),u=function(e){function t(){return e.apply(this,arguments)||this}return Object(a.a)(t,e),t.prototype.render=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"hi"),o.a.createElement(c.a,{to:"/test-page"},"Test Page!"))},t}(o.a.Component);t.default=u}}]);
-//# sourceMappingURL=component---src-pages-index-jsx-9b7db89f14bbae82765c.js.map
--- component---src-pages-test-page-jsx-5dccc7b8fca17f68e613.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{fXvc:function(t,n,e){"use strict";e.r(n);var o=e("dI71"),r=e("q1tI"),i=e.n(r),c=e("Wbzz"),u=function(t){function n(){return t.apply(this,arguments)||this}return Object(o.a)(n,t),n.prototype.render=function(){return i.a.createElement("div",null,i.a.createElement(c.a,{to:"/"},"Home"),"Looks like this is still under construction. Sorry!")},n}(i.a.Component);n.default=u}}]);
-//# sourceMappingURL=component---src-pages-test-page-jsx-5dccc7b8fca17f68e613.js.map
