514d91630d63200e6ac2807cea2900c12b6bd2b3
maxsonyang <maxsonyang@berkeley.edu>
2021-01-20 20:03:02 -0800

    Updates

---
+++ component---src-pages-test-page-jsx-b62c43592875327de1f7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{fXvc:function(t,n,e){"use strict";e.r(n);var o=e("dI71"),r=e("q1tI"),i=e.n(r),c=e("Wbzz"),u=function(t){function n(){return t.apply(this,arguments)||this}return Object(o.a)(n,t),n.prototype.render=function(){return i.a.createElement("div",null,i.a.createElement(c.a,{to:"/"},"Home"),"Looks like this is still under construction. Sorry!")},n}(i.a.Component);n.default=u}}]);
+//# sourceMappingURL=component---src-pages-test-page-jsx-b62c43592875327de1f7.js.map
