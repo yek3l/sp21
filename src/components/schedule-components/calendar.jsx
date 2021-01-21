2bfefbddd490a98ca0d880bd4bde2c50ae0fd00d
Yolanda S <yashen@berkeley.edu>
2021-01-21 10:38:44 -0800

    calendar changes

--- src/components/schedule-components/calendar.jsx
+++ src/components/schedule-components/calendar.jsx
@@ -16,23 +16,23 @@ export default class Calendar extends React.Component {
         { 
             googleCalendarId: 'berkeley.edu_k2g60q1sehd2u0ujd257jqm7h0@group.calendar.google.com',
             className : 'oh-calendar',
-            color: "#8445C2"
+            color: "#00a178"
         },
         { 
             googleCalendarId: 'berkeley.edu_1g3duo9lb53vu09orjictriud4@group.calendar.google.com',
             className : 'disc-calendar',
-            color : "#FFBB00"
+            color : "#8f56e3"
         },
         { 
             googleCalendarId: 'berkeley.edu_d358eocqj23pak3atie23vk35o@group.calendar.google.com',
             className : 'lab-calendar',
-            color: "#66894D"
+            color: "#F3B200"
         },
         { 
-            googleCalendarId: 'berkeley.edu_7qpoo4ph13p55e4ukmnpvqusdk@group.calendar.google.com',
-            className : 'lect-calendar',
-            color: "#4085E3"
-        }
+          googleCalendarId: 'berkeley.edu_7qpoo4ph13p55e4ukmnpvqusdk@group.calendar.google.com',
+          className : 'lect-calendar',
+          color: "#EE7C2F"
+      }
     ]
 
       if (isBrowser) {
